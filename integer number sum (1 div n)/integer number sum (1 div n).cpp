349273394f53f65e5f986d8d31d69d384458d10d
Nikita Polunin (二木立・半人) <Stkchanelx@gmail.com>
2021-10-20 19:06:04 +0300

    Добавьте файлы проекта.

---
+++ integer number sum (1 div n)/integer number sum (1 div n).cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int n;
+	float sum = 1;
+	float sum_temp;
+	float one = 1;
+	/*19-я строка без него почему-то не работает.
+	Думает, что 1 это int, а не float. Как это работает - не знаю, но работает */
+
+	cout << "Введите n: "; 
+	cin >> n;
+
+	for (int i = n; i > 1; i--)
+	{
+		sum_temp = one / n;
+		sum = sum + sum_temp;
+
+		n = n--;
+	}
+	cout << "Сумма равна: " << sum << endl;
+}
